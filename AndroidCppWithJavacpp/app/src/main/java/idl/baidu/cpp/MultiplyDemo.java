be2ec83904c6aa8280fee5bd43046ca619a4074a
liliqiang_baidu_mac <liliqiang@baidu.com>
2017-09-08 13:50:36 +0800

    android gradle cpp with javacpp

---
+++ AndroidCppWithJavacpp/app/src/main/java/idl/baidu/cpp/MultiplyDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package idl.baidu.cpp;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.annotation.Namespace;
+import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.StdString;
+
+@Platform(include="NativeLibrary.h",link="NativeLibrary")
+public class MultiplyDemo {
+
+    static {
+        Loader.load();
+    }
+
+    public static native int multiply(int a, int b);
+}
