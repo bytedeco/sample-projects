ff391aff81288a6a70278ba5ffe2f1e959bad2a1
liliqiang_baidu_mac <liliqiang@baidu.com>
2017-10-25 16:16:11 +0800

    update

--- javacpp-android-gradle/app/src/main/jni/Application.mk
+++
@@ -1,7 +0,0 @@
-APP_PLATFORM := android-8
-APP_ABI :=  armeabi-v7a  #armeabi x86  #armeabi-v7a  armeabi #armeabi-v7a #armeabi  #arm64-v8a x86
-APP_STL := gnustl_static
-APP_CPPFLAGS += -std=c++11 -frtti -fexceptions
-NDK_TOOLCHAIN_VERSION := 4.9
-#NDK_TOOLCHAIN_VERSION := clang
-APP_OPTIM := release
