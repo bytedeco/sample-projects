e30d56367180dfc170dfcf597a1c1b067b173688
kigkrazy <kigkrazy@gmail.com>
2018-11-08 09:50:37 +0800

    Add .gitignore and update javacv-android-recognize for OpenCV 3.4.3

--- javacv-android-recognize/app/src/main/java/org/bytedeco/javacv/android/recognize/example/MainActivity.java
+++ javacv-android-recognize/app/src/main/java/org/bytedeco/javacv/android/recognize/example/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
